90c2d6aadb8d26192e35522fb06be3f1d52807d2
Nat Welch <nat@natwelch.com>
2014-04-08 15:38:00 -0700

    debug all of the things.

--- config/boot.rb
+++ config/boot.rb
@@ -10,6 +10,7 @@ Bundler.require(:default, RACK_ENV)
 ## Enable devel logging
 Padrino::Logger::Config[:development][:log_level]  = :devel
 Padrino::Logger::Config[:development][:log_static] = true
+Padrino::Logger::Config[:production][:log_level]  = :devel
 
 ## Configure your I18n
 I18n.default_locale = :en
