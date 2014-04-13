720d979d42f3c8fa6285fb52eaa09646269066fb
Nat Welch <nat@natwelch.com>
2014-04-13 04:29:42 -0700

    make sure params are there.

--- app/controllers.rb
+++ app/controllers.rb
@@ -7,16 +7,20 @@ SnippetsApi::App.controllers  do
   post :user, :csrf_protection => false do
 
     # TODO: Add some sort of key checking.
-    hashes = JSON.parse(params[:snippets_data])
-    hashes.map do |hash|
-      snip = Snippet.find_or_create(uuid: hash.uuid)
-      snip.text = hash.text
-      snip.user_id = hash.user_id
-      snip.created = hash.created
+    if not params[:snippets_data].nil?
+      hashes = JSON.parse(params[:snippets_data])
+      hashes.map do |hash|
+        snip = Snippet.find_or_create(uuid: hash.uuid)
+        snip.text = hash.text
+        snip.user_id = hash.user_id
+        snip.created = hash.created
 
-      snip
+        snip
+      end
+      hashes.each {|s| s.save }
+    else
+      logger.push("Not a valid post request: #{params.inspect}", :warning)
     end
-    hashes.each {|s| s.save }
 
     content_type :json
     Snippet.all.to_a.to_json
