8e60951d223d169ef9881052b20b4cf8daf774a9
Nat Welch <nat@natwelch.com>
2014-04-13 10:45:30 -0700

    one last tweak

--- app/controllers.rb
+++ app/controllers.rb
@@ -8,19 +8,20 @@ SnippetsApi::App.controllers  do
   post :user, :csrf_protection => false do
 
     # TODO: Add some sort of key checking.
-    puts "Params: #{params.inspect}"
+    #puts "Params: #{params.inspect}"
     if not params["snippet_data"].nil?
       hashes = JSON.parse(params[:snippet_data]).delete_if {|a| a.nil? }
+      p hashes
       hashes = hashes.map do |hash|
-        snip = Snippet.find_or_create_by(uuid: hash["uuid"])
+        snip = Snippet.find_or_initialize_by(uuid: hash["uuid"])
         snip.text = hash["text"]
         snip.user_id = hash["user_id"]
         snip.created = hash["created"]
+        s.save
 
         snip
       end
       puts "Snippets: #{hashes.inspect}"
-      hashes.each {|s| s.save }
     else
       logger.push("Not a valid post request: #{params.inspect}", :warn)
     end
