075163ad5eb80dd5acc61d93b2b3dacb2078697d
Nat Welch <nat@natwelch.com>
2014-06-29 06:21:49 -0700

    better saving.

--- app/controllers.rb
+++ app/controllers.rb
@@ -9,18 +9,18 @@ SnippetsApi::App.controllers  do
 
     # TODO: Add some sort of key checking.
     if not params["snippet_data"].nil?
-      logger.push("recieved: #{params.inspect}", :warn)
+      logger.push("recieved: #{params.inspect}", :debug)
       hashes = JSON.parse(params[:snippet_data]).delete_if {|a| a.nil? }
-      logger.push("parsed: #{hashes.inspect}", :warn)
       hashes = hashes.map do |hash|
         logger.push("loading: #{hash.inspect}", :warn)
-        snip = Snippet.find_or_initialize_by(uuid: hash["uuid"])
-        snip.text = hash["text"]
-        snip.user_id = hash["userId"]
-        snip.created = hash["created"]
+        # loading: {"c"=>"I miss sex.", "d"=>"Jun 29, 2014 2:11:08 PM", "e"=>"nat@natwelch.com", "g"=>"android_id.15ad160c-ae73-47b4-8580-1533dc627af9"}
+        snip = Snippet.find_or_initialize_by(uuid: hash["g"])
+        snip.text = hash["c"]
+        snip.user_id = hash["e"]
+        snip.created = Chronic.parse(hash["d"])
         snip.save
 
-        logger.push("trying to save: #{snip}, #{snip.errors}", :warn)
+        logger.push("trying to save: #{snip.inspect}, #{snip.errors.inspect}", :warn)
 
         snip
       end
